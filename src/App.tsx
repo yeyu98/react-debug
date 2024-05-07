783c8ec25f1b81173cbf827bc4084b7c10540e87
yeyu98 <763525908@qq.com>
2024-05-07 21:16:02 +0800

    feat: 调试hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import AAA from './AAA';
-import logo from './logo.svg';
+import { useState, useEffect, useMemo, useCallback } from 'react';
+// import AAA from './AAA';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <AAA />
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //     </header>
+  //   </div>
+  // );
+  // 1. useState
+  const [a, setA] = useState(1);
+  // 2. useEffect
+  useEffect(() => {
+    console.log(`effect 1 created`);
+  });
+
+  // 3. useMemo
+  const memoValue = useMemo(() => a + 1, [a]);
+  // 4. useCallback
+  const fn = useCallback(() => {
+    console.log(`useCallback：${a}`)
+  }, [a])
+  // 5. useState
+  const [b] = useState(2);  
+  // 6. useEffect
+  useEffect(() => {
+    console.log(`effect 2 created`);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <AAA />
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <h1>memoValue：{memoValue}</h1>
+      <button onClick={() => setA(a + 1)}>{a}</button>
+      <button>{b}</button>
+    </>
   );
 }
 
