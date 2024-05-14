055d9628c3221f3d01e82cfd0ff3e8fdc7acf36d
yeyu98 <763525908@qq.com>
2024-05-14 11:33:07 +0800

    feat: Scheduler调试demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
+import {StrictMode} from 'react'
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import BBB from './BBB';
 import reportWebVitals from './reportWebVitals';
 
+// NOTE 调试fiber构建 classComponent
 // const App = BBB
 
+// NOTE legacy 模式下不会涉及Scheduler、时间切片、中断渲染以及恢复
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// NOTE concurrent 模式下涉及Scheduler、时间切片、中断渲染以及恢复，以此作为调试demo
+ReactDOM.createRoot(document.getElementById('root')).render(
+<StrictMode>
+  <App />
+ </StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
