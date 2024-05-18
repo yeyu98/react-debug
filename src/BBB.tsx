9679c3a25aab950d67e57611b81f31ab706204da
yeyu98 <763525908@qq.com>
2024-05-18 15:23:08 +0800

    feat: 调试classComponent beginWork的更新队列处理方式&this.setState&diff

--- src/BBB.tsx
+++ src/BBB.tsx
@@ -2,26 +2,40 @@
  * @Author: yeyu98
  * @Date: 2024-03-30 09:59:43
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-04-03 22:09:10
+ * @LastEditTime: 2024-05-18 15:22:02
  * @Description: 
  */
-import React, { PureComponent, ReactNode } from 'react'
+import { PureComponent, ReactNode } from 'react'
 
 interface Props {}
-interface State {}
+interface State {
+    list: string []
+}
 
 class BBB extends PureComponent<Props, State> {
     constructor(props: Props) {
         super(props)
 
         this.state = {
-            
+            list: ['A', 'B', 'C']
         }
+    }
+
+    handleClick = () => {
+        this.setState({
+            list: ['C', 'A', 'X']
+        })
     }
 
     render(): ReactNode {
         return (
+            <>
             <div>child BBB</div>
+            <button onClick={this.handleClick}>diff</button>
+            {
+                this.state.list.map((item, index) => (<div key={index}>{item}</div>))
+            }
+            </>
         )
     }
 }
